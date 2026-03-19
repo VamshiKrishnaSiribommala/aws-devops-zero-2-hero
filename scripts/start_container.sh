2a7a09d45402b4bed60b9882a8fbb61703662509
Vamshi krishna Siribommala <vamshikrishnasiribommala@gmail.com>
2026-03-20 01:08:05 +0530

    Simplify start_container.sh by removing redundant steps
    Removed old container stopping and image pulling steps.

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Stopping old container..."
-docker stop flask-app || true
-docker rm flask-app || true
-
-echo "Pulling latest image..."
+# Pull the latest image
 docker pull vamshikrishnasiribommala/simple-python-flask-app:latest
 
-echo "Running container..."
+# Run with a FIXED NAME so we can find it next time
 docker run -d -p 5000:5000 --name flask-app vamshikrishnasiribommala/simple-python-flask-app:latest
-
-echo "Deployment successful 🚀"
