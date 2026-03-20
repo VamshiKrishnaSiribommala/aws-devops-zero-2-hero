5c5e028cbba7dc487facae29a758f59e9d61b8a7
Vamshi krishna Siribommala <vamshikrishnasiribommala@gmail.com>
2026-03-20 23:47:50 +0530

    Simplify container stop and remove logic
    Updated script to simplify container stop and remove process.

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-# 1. Try to stop the container by name
+# Stop and remove the container if it exists
 docker stop flask-app || true
 docker rm flask-app || true
-
-# 2. Extra Safety: Kill anything else sitting on port 5000 (The "Ghost Hunter")
-container_id=$(docker ps -q --filter "ancestor=vamshikrishnasiribommala/simple-python-flask-app")
-if [ ! -z "$container_id" ]; then
-    docker stop $container_id
-    docker rm $container_id
-fi
